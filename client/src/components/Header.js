8773eb3d656ab7507ac7665b758f473d2744211e
Basia <b.kumor@yahoo.com>
2020-08-22 17:03:24 +0200

    sidenav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -6,17 +6,25 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Badge from '@material-ui/core/Badge';
+import Sidenav from './Sidenav';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div>
             <AppBar position="fixed">
                 <Toolbar>
                     <div className="container header">
                         <div className="logo">
-                            <IconButton edge="start" color="inherit" aria-label="menu">
+                            <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleNav}>
                                 <MenuIcon className="menu-icon"/>
                             </IconButton>
+                            <Sidenav isOpen={isOpen} toggleNav={toggleNav} />
                             <h1 className="title-main">awsomeShop</h1>
                         </div>
                         <div className="utils">
