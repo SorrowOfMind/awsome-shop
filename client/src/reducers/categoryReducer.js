31ae5a049d94f0b0669a335705b48b96462ebb16
Basia <b.kumor@yahoo.com>
2020-08-27 18:31:15 +0200

    initialized store

---
+++ client/src/reducers/categoryReducer.js
@@ -0,0 +1,12 @@
+const initialState = {currentCategory: 1};
+
+const categoryReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SWITCH_CAT':
+            return {...state, currentCategory: action.payload}
+        default:
+            return state;
+    }
+};
+
+export default categoryReducer;
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,11 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import rootReucer from './reducers/rootReducer';
 
 const middleware = [thunk];
 
-const store = createStore(rootReucer, compose());
+const store = createStore(rootReucer, compose(applyMiddleware(...middleware),  window.__REDUX_DEVTOOLS_EXTENSION__
+? window.__REDUX_DEVTOOLS_EXTENSION__()
+: (a) => a));
 
 export default store;
