c7f3d793dd6ef01a42b7bbc37b2022cfac2086b0
Basia <b.kumor@yahoo.com>
2020-08-29 17:51:16 +0200

    categories

---
+++ client/src/actions/types.js
@@ -0,0 +1,1 @@
+export const SWITCH_CAT = 'SWITCH_CAT';
--- client/src/reducers/categoryReducer.js
+++ client/src/reducers/categoryReducer.js
@@ -1,8 +1,10 @@
+import {SWITCH_CAT} from '../actions/types';
+
 const initialState = {currentCategory: 1};
 
 const categoryReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'SWITCH_CAT':
+        case SWITCH_CAT:
             return {...state, currentCategory: action.payload}
         default:
             return state;
