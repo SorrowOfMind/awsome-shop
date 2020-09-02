5b74ed8ac880dc7f7770b84012906b59c62447ac
Basia <b.kumor@yahoo.com>
2020-09-02 19:34:04 +0200

    welcome msg

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home container">
+            <Box  class="welcome-box clearfix">
+                <p>From the people with passion to the people with passion</p>
+                <p class="welcome-subtext">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, consectetur.</p>
+                <Button variant="contained" color="primary" className="welcome-btn">
+                    Read More
+                </Button>
+            </Box>
         </div>
     )
 }
